8d66552ac8886999e3f09a90d72151925918cc98
akshayravikumar <akshay.ravikumar@gmail.com>
2025-05-22 17:54:07 -0700

    change error code

--- apps/studio/pages/api/injection-demo.ts
+++ apps/studio/pages/api/injection-demo.ts
@@ -11,6 +11,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (error) throw error
     res.status(200).json({ data })
   } catch (err: any) {
-    res.status(500).json({ error: err.message })
+    res.status(403).json({ error: err.message })
   }
 }
